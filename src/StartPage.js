71e231efca5f44b136d6c1dbfd45f93843903f64
Azii <Mattias.johnsond@hotmail.com>
2021-05-18 19:01:38 +0200

    Startpage skeleton
    began creating the skeleton for the start page.

---
+++ src/StartPage.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function StartPage() {
+  return (
+    <div className="wrapper">
+      {/* Wheel meny component*/}
+      <h1 className="logo">Quiz-Time</h1>
+      <input className="playerNamer" placeholder="Name"></input>
+      <button className="leaderBoard">Leaderboard</button>
+      <a href="#" className="aboutUs">
+        About us
+      </a>
+    </div>
+  );
+}
