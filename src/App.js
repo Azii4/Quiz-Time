ca7c223cbdab89216c77e245ab5ea6ce70f4a195
Josefine Klintberg <jool@snel.se>
2021-05-24 22:08:33 +0200

    Installed Material UI and moved Home and About to Components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import Home from "./Home";
-import About from "./About";
-import Game from "./components/Game"
+import Home from "./components/Home";
+import About from "./components/About";
+import Game from "./components/Game";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 function App() {
   return (
