fbf4ec2e292247c74abee8dfb90ac8b5c967b107
Josefine Klintberg <jool@snel.se>
2021-05-25 11:03:13 +0200

    Added radiobuttons for category

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,8 +2,12 @@ import React, { useState } from "react";
 import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
 import Container from "@material-ui/core/Container";
-import { makeStyles } from "@material-ui/core";
+import { FormControlLabel, makeStyles } from "@material-ui/core";
 import TextField from "@material-ui/core/TextField";
+import RadioGroup from "@material-ui/core/RadioGroup";
+import Radio from "@material-ui/core/Radio";
+import FormControl from "@material-ui/core/FormControl";
+import FormLabel from "@material-ui/core/FormLabel";
 
 const useStyles = makeStyles({
   field: {
@@ -17,6 +21,7 @@ export default function Home() {
   const classes = useStyles();
   const [name, setName] = useState("");
   const [nameError, setNameError] = useState(false);
+  const [category, setCategory] = useState("movies");
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -26,7 +31,7 @@ export default function Home() {
       setNameError(true);
     }
     if (name) {
-      console.log(name);
+      console.log(name, category);
     }
   };
 
@@ -47,6 +52,35 @@ export default function Home() {
           required
           error={nameError}
         />
+
+        <FormLabel className={classes.field}>Choose Category</FormLabel>
+        <RadioGroup
+          value={category}
+          onChange={(e) => setCategory(e.target.value)}
+        >
+          <FormControlLabel value="movies" control={<Radio />} label="Movies" />
+          <FormControlLabel value="music" control={<Radio />} label="Music" />
+          <FormControlLabel
+            value="video games"
+            control={<Radio />}
+            label="Video Games"
+          />
+          <FormControlLabel
+            value="history"
+            control={<Radio />}
+            label="History"
+          />
+          <FormControlLabel
+            value="geography"
+            control={<Radio />}
+            label="Geography"
+          />
+          <FormControlLabel
+            value="animals"
+            control={<Radio />}
+            label="Animals"
+          />
+        </RadioGroup>
 
         <Button
           type="submit"
