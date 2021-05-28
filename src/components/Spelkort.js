e669f44c2104fe5db95671a263d0d4906100abd9
Josefine Klintberg <jool@snel.se>
2021-05-28 11:35:42 +0200

    no message

--- src/components/Spelkort.js
+++ src/components/Spelkort.js
@@ -1,8 +1,6 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Grid from "@material-ui/core/Grid";
-import FormLabel from "@material-ui/core/FormLabel";
-import FormControlLabel from "@material-ui/core/FormControlLabel";
 import Paper from "@material-ui/core/Paper";
 
 const useStyles = makeStyles((theme) => ({
@@ -21,10 +19,6 @@ const useStyles = makeStyles((theme) => ({
 export default function Spelkort() {
   const [spacing, setSpacing] = React.useState(2);
   const classes = useStyles();
-
-  const handleChange = (event) => {
-    setSpacing(Number(event.target.value));
-  };
 
   return (
     <Grid container className={classes.root} spacing={2} align="center">
