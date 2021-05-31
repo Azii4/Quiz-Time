a4ea0611e158d47680a641115c6483fb1366d6ec
Josefine Klintberg <jool@snel.se>
2021-05-31 20:53:17 +0200

    Revert "Renamed Spelkort to GameCards"
    This reverts commit bf8bdf32acaed00b24434c51fc7f7918b768f293.

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,67 +1,63 @@
-import "../css/game.css";
-import { useState, useEffect, useRef } from "react";
-import { getQuestions, getCategory } from "../modules/triviaApi";
-import { useLocation } from "react-router-dom";
-import { saveStandard, saveTimeAttack } from "./LocalStorage";
-import GameCards from "./GameCards";
+import "../css/game.css"
+import {useState, useEffect, useRef} from 'react'
+import {getQuestions, getCategory} from '../modules/triviaApi'
+import {useLocation} from 'react-router-dom'
+import {saveStandard, saveTimeAttack} from './LocalStorage'
+import Spelkort from "./Spelkort";
 import TimerBar from "./TimerBar";
 
 const questionAmount = 10;
 const timerTime = 10;
 
 function Game(props) {
-  const { search } = useLocation();
-  const searchParams = new URLSearchParams(search);
+    const {search} = useLocation()
+    const searchParams = new URLSearchParams(search)
 
-  const [questionList, setQuestionList] = useState([]);
-  const [questionCounter, setQuestionCounter] = useState(0);
-  const [answerList, setAnswerList] = useState([]);
-  const [isLoaded, setIsLoaded] = useState(false);
-  const [score, setScore] = useState(0);
-  const [gameOver, setGameOver] = useState(false);
-  const [startTime, setStartTime] = useState(0);
-  const [gameMode, setGameMode] = useState(searchParams.get("mode")); // Tanken är att ha 0 för standard och 1 för timeattack
-  const [name, setName] = useState(searchParams.get("name"));
-  const [category, setCategory] = useState(searchParams.get("cat"));
-  const [timer, setTimer] = useState(null);
-  const [answered, setAnswered] = useState(false);
+    const [questionList, setQuestionList] = useState([])
+    const [questionCounter, setQuestionCounter] = useState(0)
+    const [answerList, setAnswerList] = useState([])
+    const [isLoaded, setIsLoaded] = useState(false)
+    const [score, setScore] = useState(0)
+    const [gameOver, setGameOver] = useState(false)
+    const [startTime, setStartTime] = useState(0)
+    const [gameMode, setGameMode] = useState(searchParams.get('mode')) // Tanken är att ha 0 för standard och 1 för timeattack
+    const [name, setName] = useState(searchParams.get('name'))
+    const [category, setCategory] = useState(searchParams.get('cat')) 
+    const [timer, setTimer] = useState(null)
+    const [answered, setAnswered] = useState(false)
 
-  const counterValue = useRef(questionCounter);
+    const counterValue = useRef(questionCounter)
 
-  const startTimer = () => {
-    setTimer(
-      setTimeout(() => {
-        handleButton(null);
-      }, timerTime * 1000)
-    );
-  };
+    const startTimer = () => {
+        setTimer(setTimeout(() => {
+          handleButton(null)
+        }, timerTime * 1000))
+    }
 
-  useEffect(() => {
-    counterValue.current = questionCounter; // Konstig lösning för setTimeout-state problemet
-  });
+    useEffect(() => {
+        counterValue.current = questionCounter // Konstig lösning för setTimeout-state problemet
+    })
 
-  const startGame = (mode) => {
-    setIsLoaded(false);
-    setQuestionCounter(0);
-    setGameOver(false);
-    setScore(0);
-    getQuestions(questionAmount, getCategory(category))
-      .then((data) => {
-        setQuestionList(
-          data.map((elem) => ({
-            question: elem.question,
-            correctAnswer: elem.correct_answer,
-            incorrectAnswers: elem.incorrect_answers,
-          }))
-        );
-        setIsLoaded(true);
-        setStartTime(new Date());
-        if (gameMode === "1") {
-          startTimer();
-        }
-      })
-      .catch((err) => console.log(err));
-  };
+    const startGame = (mode) => {
+        setIsLoaded(false)
+        setQuestionCounter(0)
+        setGameOver(false)
+        setScore(0)
+        getQuestions(questionAmount, getCategory(category))
+        .then(data => {
+            setQuestionList(data.map(elem => ({
+                question: elem.question,
+                correctAnswer: elem.correct_answer,
+                incorrectAnswers: elem.incorrect_answers
+            })))
+            setIsLoaded(true)
+            setStartTime(new Date())
+            if(gameMode === "1") {
+                startTimer()
+            }
+        })
+        .catch(err => console.log(err))
+    }
 
   const updateAnswers = () => {
     if (questionList[0] !== undefined) {
@@ -80,55 +76,54 @@ function Game(props) {
   };
 
   useEffect(() => {
-    startGame(gameMode);
-  }, []);
+    startGame(gameMode)
+  }, [])
 
   useEffect(() => {
     updateAnswers();
   }, [questionList, questionCounter]);
 
-  const handleButton = (answer) => {
-    setAnswered(true);
-    if (answer !== null) {
-      if (answer === questionList[questionCounter].correctAnswer) {
-        setScore(score + 1);
-      }
-    }
-    setTimeout(nextQuestion, 1500);
-  };
 
-  const nextQuestion = () => {
-    setAnswered(false);
-    clearTimeout(timer);
-    if (counterValue.current < questionAmount - 1) {
-      console.log("Updating question");
-      setQuestionCounter(counterValue.current + 1);
-      updateAnswers();
-      if (gameMode === "1") {
-        startTimer();
+    const handleButton = (answer) => {
+      setAnswered(true)
+      if(answer !== null) {
+        if(answer === questionList[questionCounter].correctAnswer) {
+          setScore(score + 1)
+        }
       }
-    } else {
-      stopGame();
+      setTimeout(nextQuestion, 1500)
     }
-  };
 
-  const stopGame = () => {
-    setGameOver(true);
-    const totalTime = new Date(Date.now() - startTime)
-      .toISOString()
-      .substr(14, 5);
-    switch (gameMode) {
-      case "0":
-        saveStandard(name, category, score);
-        break;
-      case "1":
-        saveTimeAttack(name, category, score, totalTime);
-        break;
-      default:
-        break;
+    const nextQuestion = () => {
+      setAnswered(false)
+      clearTimeout(timer)
+        if(counterValue.current < questionAmount - 1) {
+            console.log("Updating question")
+            setQuestionCounter(counterValue.current + 1)
+            updateAnswers()
+            if(gameMode === "1") {
+                startTimer()
+            }
+        } else {
+            stopGame()
+        }
     }
-    clearInterval(timer);
-  };
+
+    const stopGame = () => {
+        setGameOver(true)
+        const totalTime = new Date(Date.now() - startTime).toISOString().substr(14, 5)
+        switch (gameMode) {
+            case "0":
+                saveStandard(name, category, score)
+                break;
+            case "1":
+                saveTimeAttack(name, category, score, totalTime)
+                break;
+            default:
+                break;
+        }
+        clearInterval(timer)
+    }
 
   if (gameOver) {
     return (
@@ -147,17 +142,15 @@ function Game(props) {
           <p>{questionList[questionCounter]?.question ?? "Loading..."}</p>
         </div>
       </div>
-      {isLoaded && gameMode === "1" ? (
-        <TimerBar time={timerTime} start={!answered}></TimerBar>
-      ) : null}
+      {isLoaded && gameMode === "1" ? <TimerBar time={timerTime} start={!answered}></TimerBar> : null}
       <div className="game-lower">
         <div className="answer-container">
-          <GameCards
-            answers={answerList}
-            onClick={handleButton}
-            correctAnswer={questionList[questionCounter]?.correctAnswer}
-            answered={answered}
-          />
+            <Spelkort 
+              answers={answerList} 
+              onClick={handleButton} 
+              correctAnswer={questionList[questionCounter]?.correctAnswer} 
+              answered={answered}
+            />
         </div>
       </div>
       <h1>Score: {score}</h1>
--- src/components/Spelkort.js
+++ src/components/Spelkort.js
@@ -3,6 +3,8 @@ import { makeStyles } from "@material-ui/core/styles";
 import Grid from "@material-ui/core/Grid";
 import Paper from "@material-ui/core/Paper";
 import { Typography } from "@material-ui/core";
+import Game from "./Game";
+import {useState, useEffect} from 'react'
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -15,14 +17,14 @@ const useStyles = makeStyles((theme) => ({
     alignItems: "center",
     minWidth: 100,
     minHeight: 50,
-    cursor: "pointer",
+    cursor: "pointer"
   },
   control: {
     padding: theme.spacing(2),
   },
 }));
 
-export default function GameCards(props) {
+export default function Spelkort(props) {
   const [spacing] = React.useState(2);
   const classes = useStyles();
 
@@ -33,21 +35,13 @@ export default function GameCards(props) {
           <Grid container justify="center" spacing={spacing}>
             {props.answers.map((value) => (
               <Grid key={value} item xs={6} sm={6} md={6}>
-                <Paper
+                <Paper 
                   onClick={() => {
-                    if (!props.answered) props.onClick(value);
-                  }}
-                  className={classes.paper}
-                  elevation={3}
-                  style={
-                    value === props.correctAnswer && props.answered
-                      ? {
-                          border: "2px solid green",
-                          minWidth: 96,
-                          minHeight: 46,
-                        }
-                      : null
-                  }
+                    if (!props.answered)
+                      props.onClick(value)
+                  }} 
+                  className={classes.paper} elevation={3} 
+                  style={value === props.correctAnswer && props.answered ? {border: "2px solid green", minWidth: 96, minHeight: 46} : null}
                 >
                   <Typography variant="BUTTON TEXT" color="textPrimary">
                     {value}
